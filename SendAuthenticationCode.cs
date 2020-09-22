a5150857e47dd790a5d52c670e20210fc34151e4
Evin Owen <github@evinowen.net>
2020-09-22 13:38:26 -0700

    Add logs to SendAuthenticationCode function

--- SendAuthenticationCode.cs
+++ SendAuthenticationCode.cs
@@ -18,17 +18,24 @@ namespace checkpanel_functions
         [FunctionName("SendAuthenticationCode")]
         public static void Run([QueueTrigger("send-authentication-code", Connection = "")] SendAuthenticationCodeModel item, ILogger log)
         {
+            log.LogInformation("SendAuthenticationCode Triggered");
+
             var twilio_account_sid = Environment.GetEnvironmentVariable("TWILIO_ACCOUNT_SID");
             var twilio_auth_token = Environment.GetEnvironmentVariable("TWILIO_AUTH_TOKEN");
             var twilio_sender = Environment.GetEnvironmentVariable("TWILIO_SENDER");
 
+            log.LogInformation("SendAuthenticationCode Initialized Twilio Client");
             TwilioClient.Init(twilio_account_sid, twilio_auth_token);
 
-            var message = MessageResource.Create(
+            log.LogInformation($"SendAuthenticationCode Send authentication code to ${item.Telephone}");
+
+            MessageResource.Create(
                 body: $"checkpanel: {item.Authentication}",
                 from: new Twilio.Types.PhoneNumber(twilio_sender),
                 to: new Twilio.Types.PhoneNumber(item.Telephone)
             );
+
+            log.LogInformation("SendAuthenticationCode Complete");
         }
     }
 }
